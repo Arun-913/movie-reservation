f07c78417226673dc21202104d58ffba009ada40
Arun Choudharry <arunchau986@gmail.com>
2024-07-23 12:15:07 +0530

    Delete api/dist directory

--- api/dist/types/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.PAYMENT_CONFIRM = exports.CANCEL_TICKET = exports.BOOK_SEAT = exports.SEAT_LOCK = exports.GET_SEAT = void 0;
-exports.GET_SEAT = "GET_SEAT";
-exports.SEAT_LOCK = "SEAT_LOCK";
-exports.BOOK_SEAT = "BOOK_SEAT";
-exports.CANCEL_TICKET = "CANCEL_TICKET";
-exports.PAYMENT_CONFIRM = "PAYMENT_CONFIRM";
